d33a6135ff35efcf574b7fdb252648e8508da675
Nguyễn Tấn Phát <74408785+tanphat1801@users.noreply.github.com>
2022-08-25 08:40:46 +0700

    Delete Product.js

--- src/resources/routes/Product.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const productController = require('../controllers/Product');
-
-router.get('/', productController.index);
-router.get('/:id', productController.detail);
-
-module.exports = router;
