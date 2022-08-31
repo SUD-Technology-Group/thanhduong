271de90faea29fe4f29048127bf9794a1060947e
MINHCT <trinhcamminh25112002@gmail.com>
2022-08-31 16:49:41 +0700

    update UI

--- src/public/javascript/main.js
+++ src/public/javascript/main.js
@@ -139,3 +139,13 @@ user.forEach((item) => {
 closeModalLoginButton.addEventListener('click', () => {
     modalLogin.style.display = 'none';
 });
+
+//replace main-image when click sub-image - product detail
+const mainImage = document.querySelector('.detail__container__left__img img');
+const subImages = document.querySelectorAll('.detail__container__sub__img img');
+
+subImages.forEach((image) => {
+    image.addEventListener('click', () => {
+        mainImage.setAttribute('src', image.getAttribute('src'))
+    });
+});
