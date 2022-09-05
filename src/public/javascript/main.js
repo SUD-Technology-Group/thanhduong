2eca564a755c099518832844279b8407661e5c28
MINHCT <trinhcamminh25112002@gmail.com>
2022-09-05 19:53:06 +0700

    Update footer, header, home UI

--- src/public/javascript/main.js
+++ src/public/javascript/main.js
@@ -146,6 +146,6 @@ const subImages = document.querySelectorAll('.detail__container__sub__img img');
 
 subImages.forEach((image) => {
     image.addEventListener('click', () => {
-        mainImage.setAttribute('src', image.getAttribute('src'))
+        mainImage.setAttribute('src', image.getAttribute('src'));
     });
 });
