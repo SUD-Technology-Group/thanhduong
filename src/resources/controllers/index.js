20d78d0725378a52a88d71073cf9c788985a0018
tanphat1801 <tanphat200265@gmail.com>
2022-09-06 13:47:47 +0700

    cart feature

--- src/resources/controllers/index.js
+++ src/resources/controllers/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module.exports.adminController = require('./Admin');
 module.exports.productController = require('./Product');
 module.exports.homeController = require('./Home');
@@ -9,15 +8,3 @@ module.exports.categoryController = require('./Category');
 module.exports.orderController = require('./Order');
 module.exports.cartController = require('./Cart');
 module.exports.saleController = require('./Sale');
-=======
-module.exports.adminController = require('./Admin')
-module.exports.productController = require('./Product')
-module.exports.homeController = require('./Home')
-module.exports.aboutController = require('./About')
-module.exports.contactController = require('./Contact')
-module.exports.newController = require('./New')
-module.exports.categoryController = require('./Category')
-module.exports.orderController = require('./Order')
-module.exports.cartController = require('./Cart')
-module.exports.saleController = require('./Sale')
->>>>>>> 31d47bb0ca0326ac47c42e28a94907898392934e
